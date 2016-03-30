e232bc5d133135d43aa8dc682bbc08cc1a33cb8c
Ben Ronda and Michael Schennum <mikeschenphoto@gmail.com>
2016-03-30 10:03:02 -0700

    adds delete button

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,11 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('post')
   },
+
+  actions: {
+    destroyPost(post) {
+      post.destroyPost();
+      this.transitionTo('index');
+    }
+  }
 });
