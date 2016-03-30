8367e51f05484bb55c84685116c0ec5f88700c8a
Ben Ronda and Michael Schennum <bronda95@comcast.net>
2016-03-30 09:44:41 -0700

    adds firebase

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,7 @@
 import Ember from 'ember';
 
-var posts = [{
-  id: 1,
-  title: "First Post",
-  author: "Jon Snow",
-  content: "This is the first post on the blog!",
-  image: "http://animaliaz-life.com/image.php?pic=/data_images/dog/dog7.jpg"
-}];
-
 export default Ember.Route.extend({
   model() {
-    return posts;
+    return this.store.findAll('post')
   },
 });
