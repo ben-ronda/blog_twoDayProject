b81edbd67afa67560ad54d3a648f58ae13d0b74b
Ben Ronda and Michael Schennum <mikeschenphoto@gmail.com>
2016-03-31 13:45:51 -0700

    adds delete for comments in post

--- app/routes/comment.js
+++ app/routes/comment.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  actions: {
+    destroyComment(comment) {
+      comment.destroyRecord();
+      this.transitionTo('index');
+    }
+}
 });
